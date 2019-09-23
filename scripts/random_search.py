736477a4a8fdf1408efd8c24c372191c7116c884
Natan Freitas Leite <natanfl@hotmail.com>
2019-09-23 00:36:33 -0300

    Update random_search.py

--- scripts/random_search.py
+++ scripts/random_search.py
@@ -1,64 +1,80 @@
 from random import choice, shuffle
+from time import time
 
 from numpy import arange
 
 from model import model
 
 
-var1_options = [round(num, 2) for num in arange(-5, 5, .02)]
-var2_options = [round(num, 2) for num in arange(-5, 5, .05)]
+var1_options = [round(num, 3) for num in arange(-5, 5, .02)]
+var2_options = [round(num, 3) for num in arange(-5, 5, .05)]
 
 tested_values = {}
-tolerance_reps = 10
+
+start = time()
+
+print('*** START OF RANDOM SEARCH ***')
 
 # First Round:
-var1 = choice(var1_options)
-var2 = choice(var2_options)
-actual_choice = {'var1': var1, 'var2': var2}
-actual_tested_value = model(actual_choice.values())
-tested_values['-'.join([str(num) for num in actual_choice.values()])] = actual_tested_value
+fixed_choice = {'var1': choice(var1_options), 'var2': choice(var2_options)}
+fixed_tested_value = model(fixed_choice.values())
+tested_values['-'.join([str(num) for num in fixed_choice.values()])] = fixed_tested_value
 
 optimized = False
-minimum_reps_before_stop = 0
 
 while not optimized:
-  #minimum_reps_before_stop = 0
-  before_random_columns = actual_tested_value
-  print('\nBefore random columns model value:', actual_tested_value)
-  random_columns = list(actual_choice.keys())
+  before_random_columns = fixed_tested_value
+  #print('\n[]Before random columns model value:', fixed_tested_value)
+  #print('Choices:', fixed_choice, '\n')
+  random_columns = list(fixed_choice.keys())
   shuffle(random_columns)
   for column in random_columns:
-    old_choice = actual_choice
-    old_tested_value = actual_tested_value
-    actual_variable_index = locals()[column+'_options'].index(locals()[column])
-    if actual_variable_index == 0:
-      change = 1
-    elif actual_variable_index == len(locals()[column+'_options'])-1:
-      change = -1
+    fixed_variable_index = locals()[column+'_options'].index(fixed_choice[column])
+    if fixed_variable_index == 0:
+      changes = [1]
+    elif fixed_variable_index == len(locals()[column+'_options'])-1:
+      changes = [-1]
     else:
-      change = choice([-1, 1])
-    #locals()[column] = locals()[column+'_options'][actual_variable_index+change]
-    locals()[column] = choice(locals()[column+'_options'])
-    actual_choice = {key: globals()[key] for (key, value) in actual_choice.items()}
-    if '-'.join([str(num) for num in actual_choice.values()]) not in tested_values.keys():
-      actual_tested_value = model(actual_choice.values())
-    else:
-      actual_tested_value = tested_values['-'.join([str(num) for num in actual_choice.values()])]
-    print('\nIn column loop old model value:', old_tested_value)
-    print('In column loop new model value:', actual_tested_value)
-    tested_values['-'.join([str(num) for num in actual_choice.values()])] = actual_tested_value
-    if actual_tested_value > old_tested_value:
-      actual_choice = old_choice
-      actual_tested_value = old_tested_value
-  print('\nAfter random columns model value:', actual_tested_value)
-  print('Reps', minimum_reps_before_stop)
-  if actual_tested_value == before_random_columns and minimum_reps_before_stop == tolerance_reps:
+      changes = [-1, 1]
+    for change in changes:
+      temp_choice = {key: value for (key, value) in fixed_choice.items()}
+      temp_choice[column] = locals()[column+'_options'][fixed_variable_index+change]
+      if '-'.join([str(num) for num in temp_choice.values()]) not in tested_values.keys():
+        temp_tested_value = model(temp_choice.values())
+        tested_values['-'.join([str(num) for num in temp_choice.values()])] = temp_tested_value
+      else:
+        temp_tested_value = tested_values['-'.join([str(num) for num in temp_choice.values()])]
+      #print('On change loop for {}:'.format(column), temp_tested_value)
+      #print('Choices:', temp_choice)
+      if temp_tested_value < fixed_tested_value:
+        fixed_choice = {key: value for (key, value) in temp_choice.items()}
+        fixed_tested_value = temp_tested_value
+        break
+  #print('\n[]After random columns model value:', fixed_choice)
+  #print('Choices:', fixed_tested_value, '\n')
+  if fixed_tested_value == before_random_columns:
     optimized = True
-  if actual_tested_value < before_random_columns:
-    minimum_reps_before_stop = 0
-  if actual_tested_value == before_random_columns and minimum_reps_before_stop < tolerance_reps:
-    minimum_reps_before_stop += 1
 
-optimized_choice = actual_choice
-optimized_tested_value = actual_tested_value
-print('\nFinal Value:\n-', optimized_tested_value)
+optimized_choice = fixed_choice
+optimized_tested_value = fixed_tested_value
+print('\nFinal Value:\n*', optimized_tested_value)
+
+end = time()
+
+print('\n* Elapsed time:', end-start)
+
+start = time()
+
+print('\n*** START OF GRID SEARCH ***')
+
+all_combinations = [[round(var1, 3), round(var2, 3)] for var1 in arange(-5, 5, .02) for var2 in arange(-5, 5, .05)]
+all_tested_values = [model(combination) for combination in all_combinations]
+
+optimized_tested_value = min(all_tested_values)
+optimized_choice = all_combinations[all_tested_values.index(optimized_tested_value)]
+
+print('\nFinal Value:\n*', optimized_tested_value)
+
+end = time()
+
+print('\n* Elapsed time:', end-start)
