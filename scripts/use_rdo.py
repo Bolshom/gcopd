541e051e4a8999e884691c1c57b36bfdc821b5f8
Natan Freitas Leite <natanfl@hotmail.com>
2019-09-23 21:55:15 -0300

    Update use_rdo.py

--- scripts/use_rdo.py
+++ scripts/use_rdo.py
@@ -1,3 +1,4 @@
+from math import exp, sqrt
 from random import choice, shuffle
 from time import time
 
@@ -6,13 +7,12 @@ from numpy import arange
 from random_descent_optimization import random_descent_optimization
 
 
-def model(var1, var2, var3):
-    return var1**2 + var2**2 * var3**2
+def model(var1, var2):
+    return exp(var1 + var2) * var1**2 + var2**3 * (var1+var2)**5
 
 
-var1_options = [round(num, 3) for num in arange(-5, 5, .02)]
-var2_options = [round(num, 3) for num in arange(-5, 5, .05)]
-var3_options = [round(num, 3) for num in arange(-2, 2, .25)]
+var1_options = arange(-5, 5, .02)
+var2_options = arange(-5, 5, .05)
 
 tested_values = {}
 
@@ -20,7 +20,7 @@ start = time()
 
 print('*** START OF RANDOM SEARCH ***')
 
-random_descent_optimization(model, var1=var1_options, var2=var2_options, var3=var3_options)
+random_descent_optimization(model, var1=var1_options, var2=var2_options)
 
 end = time()
 
@@ -30,8 +30,8 @@ start = time()
 
 print('\n*** START OF GRID SEARCH ***')
 
-all_combinations = [[round(var1, 3), round(var2, 3), round(var3, 3)] for var1 in arange(-5, 5, .02) for var2 in arange(-5, 5, .05) for var3 in arange(-2, 2, .25)]
-all_tested_values = [model(combination[0], combination[1], combination[2]) for combination in all_combinations]
+all_combinations = [[var1, var2] for var1 in var1_options for var2 in var2_options]
+all_tested_values = [model(combination[0], combination[1]) for combination in all_combinations]
 
 optimized_tested_value = min(all_tested_values)
 optimized_choice = all_combinations[all_tested_values.index(optimized_tested_value)]
