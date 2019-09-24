86986e7e877988e9507451852337795a79466d42
Natan Freitas Leite <natanfl@hotmail.com>
2019-09-23 22:29:14 -0300

    Update use_rdo.py

--- scripts/use_rdo.py
+++ scripts/use_rdo.py
@@ -11,33 +11,41 @@ def model(var1, var2):
     return exp(var1 + var2) * var1**2 + var2**3 * (var1+var2)**5
 
 
-var1_options = arange(-5, 5, .02)
-var2_options = arange(-5, 5, .05)
+var1_options = [round(num, 2) for num in arange(-5, 5, .02)]
+var2_options = [round(num, 2) for num in arange(-5, 5, .05)]
 
-tested_values = {}
+trials = {'random': {'result': [], 'time': []}, 'grid': {'result': [], 'time': []}}
 
-start = time()
+for trial in range(50):
 
-print('*** START OF RANDOM SEARCH ***')
+  start = time()
 
-random_descent_optimization(model, var1=var1_options, var2=var2_options)
+  print('*** START OF RANDOM SEARCH ***')
 
-end = time()
+  optimized = random_descent_optimization(model, var1=var1_options, var2=var2_options)
 
-print('\n* Elapsed time:', end-start)
+  end = time()
 
-start = time()
+  print('\n* Elapsed time:', end-start)
 
-print('\n*** START OF GRID SEARCH ***')
+  trials['random']['result'].append(optimized['value'])
+  trials['random']['time'].append(end-start)
 
-all_combinations = [[var1, var2] for var1 in var1_options for var2 in var2_options]
-all_tested_values = [model(combination[0], combination[1]) for combination in all_combinations]
+  start = time()
 
-optimized_tested_value = min(all_tested_values)
-optimized_choice = all_combinations[all_tested_values.index(optimized_tested_value)]
+  print('\n*** START OF GRID SEARCH ***')
 
-print('\n* Optimized result:', optimized_tested_value)
+  all_combinations = [[var1, var2] for var1 in var1_options for var2 in var2_options]
+  all_tested_values = [model(combination[0], combination[1]) for combination in all_combinations]
 
-end = time()
+  optimized_tested_value = min(all_tested_values)
+  optimized_choice = all_combinations[all_tested_values.index(optimized_tested_value)]
 
-print('\n* Elapsed time:', end-start)
+  print('\n* Optimized result:', optimized_tested_value)
+
+  end = time()
+
+  print('\n* Elapsed time:', end-start)
+
+  trials['grid']['result'].append(optimized_tested_value)
+  trials['grid']['time'].append(end-start)
