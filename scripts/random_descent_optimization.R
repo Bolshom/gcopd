c1fc8aae5368cc12ff2ebf84664da0627e21a672
Natan Freitas Leite <natanfl@hotmail.com>
2019-09-23 23:51:22 -0300

    New Files in R

--- scripts/random_descent_optimization.R
+++ scripts/random_descent_optimization.R
@@ -1,46 +1,17 @@
+source('rdo_jump.R', encoding='utf-8')
+
+
 random_descent_optimization = function(f, ...){
-  options = list(...)
-  tested_values = list()
+  reps = 10
+  pre_results = list()
+  for (trial in 1:reps) pre_results[[trial]] = rdo_jump(f, ...)
   
-  # First round:
-  fixed_choice = list()
-  for (variable in names(options)){
-    fixed_choice[[variable]] = sample(options[[variable]], 1)
-  }
-  fixed_tested_value = do.call(f, fixed_choice)
-  tested_values[[paste(fixed_choice, collapse='-')]] = fixed_tested_value
+  values = sapply(pre_results, function(result) result[['value']])
   
-  optimized = FALSE
-  
-  while (!optimized){
-    before_random_columns = fixed_tested_value
-    for (column in sample(names(options))){
-      fixed_variable_index = which(options[[column]] == fixed_choice[[column]])
-      if (fixed_variable_index == 1){
-        changes = 1
-      } else if (fixed_variable_index == length(options[[column]])) {
-        changes = -1
-      } else changes = c(-1, 1)
-      for (change in changes){
-        temp_choice = fixed_choice
-        temp_choice[[column]] = options[[column]][fixed_variable_index+change]
-        if (!(paste(temp_choice, collapse='-') %in% names(tested_values))){
-          temp_tested_value = do.call(f, temp_choice)
-          tested_values[[paste(temp_choice, collapse='-')]] = temp_tested_value
-        } else temp_tested_value = tested_values[[paste(temp_choice, collapse='-')]]
-        if (temp_tested_value < fixed_tested_value){
-          fixed_choice = temp_choice
-          fixed_tested_value = temp_tested_value
-          break
-        }
-      }
-    }
-    if (fixed_tested_value == before_random_columns) optimized = TRUE
-  }
-  
-  optimized_choice = fixed_choice
-  optimized_tested_value = fixed_tested_value
+  optimized_tested_value = min(values)
+  optimized_choice = pre_results[[which.min(values)]][['choices']]
   
   cat('\n* Optimized result:', optimized_tested_value, '\n')
-  return(optimized_choice)
+  
+  return(list('value'=optimized_tested_value, 'choices'=optimized_choice))
 }
--- scripts/use_rdo.R
+++ scripts/use_rdo.R
@@ -1,23 +1,34 @@
-source('/basedir/scripts/random_descent_optimization.R', encoding='utf-8')
+source('random_descent_optimization.R', encoding='utf-8')
 
 
 model = function(var1, var2){
-  return(var1^2 + var2^2)
+  return(exp(var1+var2) * var1^2 + var2^3 * (var1+var2)^5)
 }
 
 
 var1_options = seq(-5, 5, .02)
 var2_options = seq(-5, 5, .05)
 
+trials = list(
+  'random'=list('result'=c(), 'time'=c()),
+  'grid'=list('result'=c(), 'time'=c())
+)
+
+
+for (trial in 1:10){
+
 start = Sys.time()
 
 cat('*** START RANDOM SEARCH ***\n')
 
-optimized_result = random_descent_optimization(model, var1=var1_options, var2=var2_options)
+optimized = random_descent_optimization(model, var1=var1_options, var2=var2_options)
 
 end = Sys.time()
 
 cat('\n* Elapsed time:', end-start, '\n')
+
+trials[['random']][['result']] = c(trials[['random']][['result']], optimized[['value']])
+trials[['random']][['time']] = c(trials[['random']][['time']], end-start)
 
 start = Sys.time()
 
@@ -39,4 +50,11 @@ cat('\n* Optimized result:', optimized_tested_value, '\n')
 
 end = Sys.time()
 
-cat('\n* Elapsed time:', end-start)
+cat('\n* Elapsed time:', end-start, '\n')
+
+trials[['grid']][['result']] = c(trials[['grid']][['result']], optimized_tested_value)
+trials[['grid']][['time']] = c(trials[['grid']][['time']], end-start)
+}
+
+cat('\nMean time for Random Search:', mean(trials[['random']][['time']]), '\n')
+cat('\nMean time for Grid Search:', mean(trials[['grid']][['time']]), '\n')
