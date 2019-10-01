406aaaee83d06e60562f394a31e8fad2619e11cb
Natan Freitas Leite <natanfl@hotmail.com>
2019-10-01 01:15:34 +0000

    New packages

--- scripts/requirements.R
+++ scripts/requirements.R
@@ -5,6 +5,7 @@ install.packages(c('devtools',
                    'magrittr',
                    'ggplot2',
                    'xgboost',
+                   'snow',
                    'zoo'))
 
 require(devtools)
